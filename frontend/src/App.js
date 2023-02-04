44fc27546d3202abd3588407dcbdbcd7c3b28789
Preet Shah <p.shah2@newcastle.ac.uk>
2023-02-04 14:49:25 +0000

    header footer created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Futourist</h1>
-    </div>
+    <>
+
+        <div className="navbar bg-primary text-primary-content">
+          <a
+            href="/"
+            aria-label="Welcome to Futourist"
+            className="btn btn-ghost normal-case text-xl"
+          >
+            FuTourist
+          </a>
+        </div>
+
+        <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+          <div className="items-center grid-flow-col">
+            <p>Copyright © 2023 - All right reserved</p>
+          </div>
+          <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <p>Made with &hearts; by Akanksha Tiwari & Preet Shah for Hack for the Future</p>
+          </div>
+        </footer>
+  
+    </>
   );
 }
+
 export default App;
