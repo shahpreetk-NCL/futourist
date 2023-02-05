50f7fc2a84d46c82001837192cd89ea62b7b1b35
Preet Shah <p.shah2@newcastle.ac.uk>
2023-02-05 14:04:04 +0000

    frontend code for deploying

--- backend/index.js
+++
@@ -1,5 +0,0 @@
-const app = require("./app");
-const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
